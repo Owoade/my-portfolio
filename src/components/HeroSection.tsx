d2f44e5c8333d54a39a847ed792e8c104fff4fb3
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2022-09-30 17:32:11 -0700

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,16 +15,22 @@ function HeroSection() {
         </Text>
 
         <HStack fontSize={{xs:"50px", base:"25px"}} width={{xs:"280px", base:"180px"}} margin="0 auto">
-            <RiTwitterFill display={"block"} />
+            <a href="https://twitter.com/Owoade_" target={"blank"}>
+             <RiTwitterFill display={"block"} />
+            </a>
             <Spacer />
-            <RiLinkedinBoxFill display={"block"} />
+            <a href="https://www.linkedin.com/in/owoade-anuoluwapo-a09a03235?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BZGduMSecRgqYrJnRpimadg%3D%3D" target={"blank"}>
+              <RiLinkedinBoxFill display={"block"} />
+            </a>
             <Spacer />
-            <RiGithubFill display={"block"} />
+            <a href="https://github.com/Owoade" target={"blank"}>
+              <RiGithubFill display={"block"} />
+            </a>
         </HStack>
 
         <Image margin={"2em auto"} width={{xs:"300px", base:"220px"}} clipPath={"circle(41% at 50% 50%)"} src="https://res.cloudinary.com/dles2mycv/image/upload/v1664575318/IMG-20220901-WA0009-graysclale_xh2xya.jpg" />
 
-        <HStack margin={"1em auto"} display={{xs:"flex", base:"block"}}  width="fit-content" transform={{sm:"translateY(2em)", base:"translateY(.5em)"}}>
+        <HStack margin={"1em auto"} display={{xs:"flex", base:"block"}}  width={{xs:"fit-content", base:"100%"}} transform={{sm:"translateY(2em)", base:"translateY(.5em)"}}>
             <Text margin="1em 0" >Wanna see what I'm capable of? </Text>
             <Button className='main-cta' variant={"primary"}> View Portfolio 🔥</Button>
         </HStack>
