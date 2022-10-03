5a4578760fd0f86849d240e54161e530eaea4649
Anuoluwapo <63651250+Owoade@users.noreply.github.com>
2022-10-03 13:33:12 -0700

    added resume link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
             Owoade Anu<chakra.span display={{xs:"inline-block", base: "none"}} fontSize={{xs:"30px", base: "25px"}} fontFamily={"Sacramento, cursive"}>oluwapo</chakra.span>
         </Box>
         <Spacer />
-        <Button variant="primary">
+        <Button variant="primary" as="a" href="https://docs.google.com/document/d/10PvC_xcqdgltl8Kt_lIFVg0J7cKMY18fxSz9sC8yATk/edit?usp=sharing" target={"_blank"}>
             Resume
         </Button>
     </Flex>
